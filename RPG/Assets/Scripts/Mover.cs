e1c79a5f59dc79fe0d36ee67b1a92631b3e9248d
eliebeaino <eliebeaino89@gmail.com>
2020-07-31 12:44:26 +0300

    Sandbox scene(ignore previous)
    Sandbox introduced with mouse to click move

---
+++ RPG/Assets/Scripts/Mover.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    private Ray lastRay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool hasHit = Physics.Raycast(ray, out hit);
+
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = new Vector3(hit.point.x, hit.point.y, hit.point.z);
+        }
+    }
+}
