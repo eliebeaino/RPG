e8e9bced10c3f8f1a9297fd6d539079fbb1889c1
eliebeaino <eliebeaino89@gmail.com>
2020-08-03 18:31:49 +0300

    fading between scenes
    - new folder for scene management scripts and namespace
    - Added Fader in between Scene
    - Added Prefab for core

---
+++ RPG/Assets/Scripts/Core/PersistentObjectSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class PersistentObjectSpawner : MonoBehaviour
+    {
+        [SerializeField] GameObject persistentObjectPrefab;
+
+        static bool hasSpawned = false;
+
+        private void Awake()
+        {
+            if (hasSpawned) return; // allows for only one to spawn by checking the static bool
+            hasSpawned = true;
+
+            SpawnPersistentObjects();
+        }
+
+        // spawn root object of persistant objects and add don't destory on load
+        private void SpawnPersistentObjects()
+        {
+            GameObject gameObject = Instantiate(persistentObjectPrefab);
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
--- RPG/Assets/Scripts/Scene Management/Portal.cs
+++ RPG/Assets/Scripts/Scene Management/Portal.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
-namespace RPG.Core
+namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
@@ -13,10 +13,17 @@ namespace RPG.Core
             A,B,C,D,E
         }
 
-        int currentScene;
+        [Header("Scene Propreties")]
         [SerializeField] [Range (-1,1)] int sceneToLoad = 1;
         [SerializeField] Transform spawnPoint;
         [SerializeField] DestinationIdentifier destination;
+
+        [Header("Fading Propreties")]
+        [SerializeField] float fadeOutTime = 3f;
+        [SerializeField] float fadeInTime = 1f;
+        [SerializeField] float pauseTimeBetweenFades = 1f;
+
+        int currentScene;
 
         private void Start()
         {
@@ -32,16 +39,22 @@ namespace RPG.Core
             }
         }
 
-        // scene transition... loads the next scene in the correct spawn point
+        // loads the next scene in the correct spawn point --- fades out -> pause -> fade in new scene
         IEnumerator Transition()
         {
-            DontDestroyOnLoad(this.gameObject);
-            yield return SceneManager.LoadSceneAsync(currentScene + sceneToLoad);
+            DontDestroyOnLoad(gameObject); // preserves the object through the scene transition
 
+            Fader fader = FindObjectOfType<Fader>();
+            yield return fader.FadeOut(fadeOutTime);            
+            yield return SceneManager.LoadSceneAsync(currentScene + sceneToLoad);
+            
             Portal otherPortal = GetOtherPortal();
             UpdatePlayer(otherPortal);
 
-            Destroy(this);
+            yield return new WaitForSeconds(pauseTimeBetweenFades);
+            yield return fader.FadeIn(fadeInTime);
+
+            Destroy(gameObject); // remove this portal from the new loaded scene as we no longer need it
         }
 
         // spawns the player in the spawn point of the new scene spawn point location - preserve ( position & rotation )
@@ -52,7 +65,7 @@ namespace RPG.Core
             player.transform.rotation = otherPortal.spawnPoint.rotation;
         }
 
-        // scans for other portals in scene and grabs the new one
+        // scans for other portals in scene and grabs the new one - uses singleton
         private Portal GetOtherPortal()
         {
             foreach (Portal portal in FindObjectsOfType<Portal>())
